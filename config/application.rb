4abb87b19469d23fa5a2208e32b7a73a2a93bfd8
konyan <devkonyan@gmail.com>
2020-04-01 15:45:38 +0630

    change default layout to custom

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,10 @@ module Tayar
 
     config.to_prepare do
       Devise::SessionsController.layout "login"
+      Devise::RegistrationsController.layout "login"
+      Devise::ConfirmationsController.layout "login"
+      Devise::UnlocksController.layout "login"
+      Devise::PasswordsController.layout "login"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2020_03_30_191736) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password_digest"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
